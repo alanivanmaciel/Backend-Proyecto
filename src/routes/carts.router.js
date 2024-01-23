ec894944c7c6b750befee1a58ec89630e2a2b123
Alan Maciel <alan.ivan.maciel@gmail.com>
2024-01-23 11:48:33 -0300

    carts

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,50 +1,49 @@
 import Router from "express";
-import CartsManager from "../daos/FileSystem/cartsManagerFS.js";
 import cartsModel from "../daos/models/carts.models.js";
 
 const router = Router();
 const cartsmodel = new cartsModel();
 
-const cartsService = new CartsManager();
-
-
 router
-.get("/:cid", async (req, res) => {
-  try {
-    const { cid } = req.params;
-    const cart = await cartsService.getCartById(parseInt(cid));
-    res.send({
-      status: "succes",
-      payload: cart,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-})
-.post("/", async (req, res) => {
-  try {
-    const result = await cart.createCart();
-    res.send({
-      status: "succes",
-      payload: result,
-    });
-  } catch (error) {
-    res.status(500).send(`Error de servidor. ${error.message}`);
-  }
-})
+  .get("/:cid", async (req, res) => {
+    try {
+      const { cid } = req.params;
+      const cart = await cartsModel.findById(cid)
+      res.send({
+        status: "succes",
+        payload: cart,
+      });
+    } catch (error) {
+      res.status(500).send(`Error de servidor. ${error.message}`);
+    }
+  })
 
-.post("/:cid/product/:pid", async (req, res) => {
-  //const { cid, pid } = req.params;
-  const { product, quantity } = req.params;
-  console.log(typeof cartsModel);
-  
-  await cartsmodel.create({
-    product: [{product, quantity}]
-  });
-    res.send({
-      status: "succes",
-      cart,
-    });
+  .post("/", async (req, res) => {
+    try {
+      const result = await cartsModel.create();
+      console.log("funca");
+      res.send({
+        status: "succes",
+        payload: result,
+      });
+    } catch (error) {
+      res.status(500).send(`Error de servidor. ${error.message}`);
+    }
+  })
+
+  .post("/:cid/product/:pid", async (req, res) => {
+    try {
+      const { product, quantity } = req.body;
+      const cart = await cartsModel.create({
+        product: [{ product, quantity }],
+      });
+      res.send({
+        status: "succes",
+        cart,
+      });
+    } catch (error) {
+      res.status(500).send(`Error de servidor. ${error.message}`);
+    }
   });
 
 export default router;
